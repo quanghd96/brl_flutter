50c5f00fcd6e24dd80f6fb9df50947eaba8372c7
Dang Ngoc Quang <quanghd96@gmail.com>
2020-12-17 14:07:40 +0700

    add startConfig and cancelConfig on ios

--- ios/Classes/BroadlinkFlutterPlugin.h
+++ ios/Classes/BroadlinkFlutterPlugin.h
@@ -1,4 +1,6 @@
 #import <Flutter/Flutter.h>
+#import <BLLetBase/BLLetBase.h>
+#import <BLLetCore/BLLetCore.h>
 
 @interface BroadlinkFlutterPlugin : NSObject<FlutterPlugin>
 @end
--- ios/Classes/BroadlinkFlutterPlugin.m
+++ ios/Classes/BroadlinkFlutterPlugin.m
@@ -10,8 +10,26 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"startConfig" isEqualToString:call.method]) {
+      NSString *ssidName = call.arguments[@"ssid"];
+      NSString *password = call.arguments[@"password"];
+      dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                  NSDate *date = [NSDate date];
+                  NSLog(@"====Start Config===");
+                  BLDeviceConfigResult *res = [[BLLet sharedLet].controller deviceConfig:ssidName password:password version:3 timeout:60];
+                  NSLog(@"====Config over! Spends(%fs)", [date timeIntervalSinceNow]);
+                  dispatch_async(dispatch_get_main_queue(), ^{
+                      if ([res succeed]) {
+                          result(@"Configure Wi-Fi success");
+                      } else {
+                          result(@"Configure Wi-Fi error");
+                      }
+                      
+                  });
+              });
+  } else if ([@"cancelConfig" isEqualToString:call.method]) {
+      
+      result(@"ok");
   } else {
     result(FlutterMethodNotImplemented);
   }
